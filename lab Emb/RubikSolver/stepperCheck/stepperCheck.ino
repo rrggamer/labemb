820d9b71bae66b877c998f328214be086dd89a75
IXVIA <phakaewpra@gmail.com>
2025-02-25 15:16:45 +0700

    Update StepperCheck

--- lab Emb/RubikSolver/stepperCheck/stepperCheck.ino
+++ lab Emb/RubikSolver/stepperCheck/stepperCheck.ino
@@ -1,35 +1,41 @@
-const int dirPin = 12;
-const int stepPin = 14;
-const int enablePin = 27;
+const int dirPins[] = {12, 14, 27, 26, 25, 33};  // Define unique DIR pins
+const int stepPins[] = {4, 16, 17, 5, 18, 19};
+const int enablePin = 34;   // Define unique STEP pins
 
 void setup() {
-  pinMode(enablePin, OUTPUT);
-  pinMode(stepPin, OUTPUT);
-  pinMode(dirPin, OUTPUT);
+  for (int i = 0; i < 6; i++) {
+    pinMode(dirPins[i], OUTPUT);
+    pinMode(stepPins[i], OUTPUT);
 
-  digitalWrite(enablePin, LOW);
+  }
+   
+   pinMode(enablePin,OUTPUT);
+   digitalWrite(enablePin,LOW);
+
 }
 
 void loop() {
-  const int steps = 200;
-  const int delayMicros = 500;
-  const int delayMillis = 1000;
+  const int steps = 200;       // Steps per cycle
+  const int delayMicros = 500; // Microsecond delay between steps
+  const int delayMillis = 1000; // Millisecond delay after each move
 
-  digitalWrite(dirPin, HIGH);
-  for (int i = 0; i < steps; i++) {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(delayMicros);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(delayMicros);
-  }
-  delay(delayMillis);
+  for (int motor = 0; motor < 6; motor++) {
+    digitalWrite(dirPins[motor], HIGH); // Set direction CW
+    for (int i = 0; i < steps; i++) {
+      digitalWrite(stepPins[motor], HIGH);
+      delayMicroseconds(delayMicros);
+      digitalWrite(stepPins[motor], LOW);
+      delayMicroseconds(delayMicros);
+    }
+    delay(delayMillis);
 
-  digitalWrite(dirPin, LOW);
-  for (int i = 0; i < steps; i++) {
-    digitalWrite(stepPin, HIGH);
-    delayMicroseconds(delayMicros);
-    digitalWrite(stepPin, LOW);
-    delayMicroseconds(delayMicros);
+    digitalWrite(dirPins[motor], LOW); // Set direction CCW
+    for (int i = 0; i < steps; i++) {
+      digitalWrite(stepPins[motor], HIGH);
+      delayMicroseconds(delayMicros);
+      digitalWrite(stepPins[motor], LOW);
+      delayMicroseconds(delayMicros);
+    }
+    delay(delayMillis);
   }
-  delay(delayMillis);
 }
