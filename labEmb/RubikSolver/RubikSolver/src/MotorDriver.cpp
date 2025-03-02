78d2af24060e87b79201562bb9905ddd4aeace1f
IXVIA <phakaewpra@gmail.com>
2025-03-02 18:40:12 +0700

    edit pin

--- labEmb/RubikSolver/RubikSolver/src/MotorDriver.cpp
+++ labEmb/RubikSolver/RubikSolver/src/MotorDriver.cpp
@@ -1,27 +1,31 @@
 #include <Arduino.h>
 
-const int dirPins[] = {12, 14, 27, 26, 25, 33};  
-const int stepPins[] = {4, 16, 17, 5, 18, 19};
+// const int stepPins[] = {4, 16, 17, 5, 18, 19};
+// const int dirPins[] = {12, 14, 27, 26, 25, 33};  
 
-#define STEP_PIN_1  4   // Motor 1 (U)
-#define DIR_PIN_1   12    
+const int stepPins[] = {33, 19, 26, 5, 14, 16};
+const int dirPins[] = {25, 18, 27, 17, 12, 4};  
 
-#define STEP_PIN_2  16   // Motor 2 (R)
-#define DIR_PIN_2   14  
 
-#define STEP_PIN_3  17   // Motor 3 (F)
+#define STEP_PIN_1  33   // Motor 1 (U)
+#define DIR_PIN_1   25    
+
+#define STEP_PIN_2  19   // Motor 2 (R)
+#define DIR_PIN_2   18  
+
+#define STEP_PIN_3  26   // Motor 3 (F)
 #define DIR_PIN_3   27  
 
 #define STEP_PIN_4  5   // Motor 4 (D)
-#define DIR_PIN_4   26  
+#define DIR_PIN_4   17  
 
-#define STEP_PIN_5  18  // Motor 5 (L)
-#define DIR_PIN_5   25  
+#define STEP_PIN_5  14  // Motor 5 (L)
+#define DIR_PIN_5   12  
 
-#define STEP_PIN_6  19  // Motor 6 (B)
-#define DIR_PIN_6   33  
+#define STEP_PIN_6  16  // Motor 6 (B)
+#define DIR_PIN_6   4  
 
-const int enablePin = 34;
+const int enable_motor5 = 35;
 
 void moveMotor(int stepPin, int dirPin, bool clockwise, int steps) {
   digitalWrite(dirPin, clockwise ? HIGH : LOW);
@@ -48,6 +52,7 @@ void L2() { L(); L(); }
 void B2() { B(); B(); }
 
 void solveSequence() {
+
   U();
   delay(2000);
 
@@ -55,24 +60,54 @@ void solveSequence() {
   delay(2000);
 
   Move_F();
+  delay(2000);
   
-  // delay(2000);
-  // D();
-  // delay(2000);
-  // L();
-  // delay(2000);
-  // B();
-  // delay(2000);
+  D();
+  delay(2000);
+  
+  digitalWrite(enable_motor5, LOW);
+
+  L();
+  delay(2000);
+  
+  B();
+  delay(2000);
+
+
+  B(true);
+  delay(2000);
+
+  L(true);
+  delay(2000);
+
+  digitalWrite(enable_motor5, HIGH);
+
+
+  D(true);
+  delay(2000);
+
+  Move_F(true);
+  delay(2000);
+
+  R(true);
+  delay(2000);
+
+  U(true);
+  delay(2000);
+
 }
 
 void setup() {
+
+  delay(3000);
+  
   for (int i = 0; i < 6; i++) {
     pinMode(stepPins[i], OUTPUT);
     pinMode(dirPins[i], OUTPUT);
   }
 
-  pinMode(enablePin, OUTPUT);
-  digitalWrite(enablePin, LOW);
+  pinMode(enable_motor5, OUTPUT);
+  digitalWrite(enable_motor5, HIGH);
 
   delay(1000);
   solveSequence();
